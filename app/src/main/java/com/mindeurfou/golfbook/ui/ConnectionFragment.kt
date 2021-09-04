c0b59ef9129c6932e0611208839e6145c42153f4
MindeurFou <tanguy.pouriel@utt.fr>
2021-09-04 11:38:00 +0200

    Adding frontend ressources

---
+++ app/src/main/java/com/mindeurfou/golfbook/ui/ConnectionFragment.kt
@@ -0,0 +1,35 @@
+package com.mindeurfou.golfbook.ui
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mindeurfou.golfbook.R
+import com.mindeurfou.golfbook.databinding.FragmentConnectionBinding
+import com.mindeurfou.golfbook.databinding.FragmentSplashBinding
+import com.mindeurfou.golfbook.ui.common.BaseFragment
+
+class ConnectionFragment : BaseFragment(R.layout.fragment_splash) {
+
+    private var _binding: FragmentConnectionBinding? = null
+
+    private val binding
+        get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentConnectionBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
