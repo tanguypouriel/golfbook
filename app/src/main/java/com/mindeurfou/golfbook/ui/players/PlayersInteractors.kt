f62913a1c1349b80eed1fbdf170e6b6c35c58b28
MindeurFou <tanguy.pouriel@utt.fr>
2021-10-17 11:34:27 +0200

    correct playersInteractors versions

--- app/src/main/java/com/mindeurfou/golfbook/ui/players/PlayersInteractors.kt
+++ app/src/main/java/com/mindeurfou/golfbook/ui/players/PlayersInteractors.kt
@@ -16,18 +16,11 @@ class PlayersInteractors
 
         emit(DataState.Loading)
 
-        kotlinx.coroutines.delay(1000)
-
-        val players = listOf(
-            Player(1, "Lisa le boss", "De la Fleche", "Petrouchka", 1),
-            Player(2, "Lisa le boss", "De la Fleche", "Petrouchka", 1),
-            Player(3, "Lisa le boss", "De la Fleche", "Petrouchka", 1),
-            Player(4, "Lisa le boss", "De la Fleche", "Petrouchka", 1),
-            Player(5, "Lisa le boss", "De la Fleche", "Petrouchka", 1),
-            Player(6, "Lisa le boss", "De la Fleche", "Petrouchka", 1),
-            Player(7, "Lisa le boss", "De la Fleche", "Petrouchka", 1),
-        )
-
-        emit(DataState.Success(players))
+        try {
+            val players = playerNetworkDataSourceImpl.getPlayers()
+            emit(DataState.Success(players))
+        } catch (e: Exception) {
+            emit(DataState.Failure(e))
+        }
     }
 }
